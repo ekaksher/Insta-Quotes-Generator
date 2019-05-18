52d4e68bf1fce6f7317c16000489c629c51d147b
Tabish <taabishm2@gmail.com>
2019-05-18 10:41:27 +0530

    Improved readability

--- instagram_quotes_generator.py
+++ instagram_quotes_generator.py
@@ -1,25 +1,28 @@
-"""This is a simple script used to scrape a random quote from internet.
-And Create An Instagram Format Post For The Same."""
+"""This is a simple script used to scrape a random quote from internet and Create An Instagram Format Post For The Same."""
+
 import requests
 from bs4 import BeautifulSoup
 from PIL import Image, ImageDraw, ImageFont
-import matplotlib.font_manager as fm
-from instapy_cli import client
+import matplotlib.font_manager as fm            #Matplotlib Font Manager
+from instapy_cli import client                  #Instagram upload interface
 
 def generate_pic(text_color=(225, 225, 225), background_color=(0, 0, 0)):
     """This function takes the quote and creates an image for the same
     It uses standard instagram size and can take an colour background.
     It can also take colour for text also. """
+    
     # (612,612) is used for instagram format size.
     img = Image.new('RGB', (612, 612), color=background_color)
     draw_image = ImageDraw.Draw(img)
     selected_font = ImageFont.truetype(fm.findfont(fm.FontProperties(family='cinzel.otf')),30)
     sum_quote = 0
+    
     # this loops gets textsize of every letter in the quotes and adds it.
     for letter in QUOTE:
         sum_quote += draw_image.textsize(letter, font=selected_font)[0]
     average_length_of_letter = sum_quote/len(QUOTE)
     number_of_letters_for_each_line = (612/1.618)/average_length_of_letter
+    
     incrementer = 0
     fresh_sentence = ''
     # creates a fresh sentense in proper format.
@@ -36,6 +39,7 @@ def generate_pic(text_color=(225, 225, 225), background_color=(0, 0, 0)):
                 fresh_sentence += letter
         incrementer += 1
     print(fresh_sentence)
+    
     dim = draw_image.textsize(fresh_sentence, font=selected_font)
     x_2 = dim[0]
     y_2 = dim[1]
@@ -54,17 +58,20 @@ def post_img(uname,pwd,caption="Quote Upload"):
 def scrape_quote():
     """This function scrapes a random quote from internet along with its
      author and tags related to it. """
+    
     res = requests.get("http://quotes.toscrape.com/random", timeout=5)
     soup = BeautifulSoup(res.content, "html.parser")
     quote_text = soup.find("span", attrs={"class":"text"}).text
     author = soup.find("small", attrs={"class":"author"}).text
+    
     #tags to used later on.
     # tags_soup = soup.find_all("a", attrs={"class":"tag"})
     # tags_list = [x.text for x in tags_soup]
+    
     data = [quote_text, author]
     return data
 
 QUOTE = (" ~ ").join(scrape_quote())
-print(QUOTE)
 generate_pic()
+
 post_img('hakovut','1qaz2wsx','First Pic #Test')
